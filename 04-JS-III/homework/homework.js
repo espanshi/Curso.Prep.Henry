98c4868b6082d719aefb57a54f0673d6b4899c45
espanshi <mateobazan@hotmail.com>
2022-02-05 09:55:27 -0300

    tarea 4

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -67,7 +67,7 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  for (i = 0; i < array.length; i++){
+  for (var i = 0; i < array.length; i++){
     if (array[i] === elemento){
       return true;
     }
@@ -82,7 +82,7 @@ function agregarNumeros(numeros) {
   // Tu código:
   var suma = 0
   for (i = 0; i < numeros.length; i++){
-    suma =+ numeros[i];
+    suma = suma + numeros[i];
   }
   return suma;
 }
@@ -92,9 +92,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  var sum = agregarNumeros(resultadosTest);
-  var prom = sum / resultadosTest.length;
-  return prom;
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
 
@@ -104,7 +102,7 @@ function numeroMasGrande(numeros) {
   // Tu código:
   var grande = numeros[0];
   for (i = 0; i < numeros.length; i++){
-    if (numeros[i] >= grande){
+    if (numeros[i] > grande){
       grande = numeros[i];
     }
   }
@@ -129,8 +127,8 @@ function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
 var contador = 0;
-for (i = 0; i < arreglo.length; i++){
-  if (arreglo[i] > 18){
+for (var i = 0; i < arreglo.length; i++){
+  if (arreglo[i] > 19){
     contador++;
   }
 }
@@ -143,7 +141,7 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí
-  if (numeroDeDia === 1 || numeroDeDia=== 7){
+  if (numeroDeDia === 1 || numeroDeDia === 7){
     return "Es fin de semana";
   }
   else {
@@ -156,8 +154,8 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
- let num = n.toString()
- if (num.charAt[0] === "9"){
+ let num = n.toString();
+ if (num[0] === "9"){
    return true;
  }
  return false;
@@ -167,13 +165,19 @@ function empiezaConNueve(n) {
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var res = false;  
+  var base = arreglo[0];
   for (i = 0; i < arreglo.length; i++){
-    if (arreglo[i] != arreglo[0]){
-      return false;
+    if (base === arreglo[i]){
+      res = true;
+    }
+    else {
+      res = false;
+      break;
     }
   }
-  return true;
+  return res;
 } 
 
 
@@ -220,11 +224,12 @@ function todosIguales(arreglo) {
   //Pista: usá el statement 'break'
   // Tu código:
   let array = [];
-  for (i = 0; i < 10; i++){
-    numero =+ 2;
-    array.push(numero);
-    if (numero === i){
-      break;
+  var suma = numero;
+  for (var i = 0; i < 10; i++){
+    suma = suma + 2;
+    if (suma === i) break;
+    else {
+      array.push(suma);
     }
   }
   if (i < 10){
@@ -244,13 +249,14 @@ function todosIguales(arreglo) {
   //Pista: usá el statement 'continue'
   // Tu código:
   let array = [];
+  let suma = numero;
   for (i = 0; i < 10; i++){
-    if (i = 5){
+    if (i === 5){
       continue;
     }
     else {
-      numero =+ 2;
-      array.push(numero);
+      suma = suma + 2;
+      array.push(suma);
     }
   }
   return array;
